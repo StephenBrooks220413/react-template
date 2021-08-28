491b6168f39321c6eac0394e9d037920d238e3c5
Stephen A Brooks <krammer439298@gmail.com>
2021-08-28 15:34:36 -0700

    added files

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function Footer() {
+  return (
+    <>
+      <div className="container">
+        <hr />
+        <p>
+            React Developer: Stephen Brooks
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
